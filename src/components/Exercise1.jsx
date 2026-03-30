0078c7ff70a8347a87460fb8c90557c7fde7b96d
koguchi-e <koguchi.e915@gmail.com>
2026-03-30 14:48:58 +0900

    表示する名前を18歳以上に変更

--- src/components/Exercise1.jsx
+++ src/components/Exercise1.jsx
@@ -2,7 +2,7 @@ import { users } from "../data/data";
 
 const Exercise1 = () => {
   const listUsers = users.map((user) => {
-    if (user.age > 18) {
+    if (user.age >= 18) {
       return <li key={user.id}>{user.name}（成人）</li>;
     } else {
       return <li key={user.id}>{user.name}</li>;
