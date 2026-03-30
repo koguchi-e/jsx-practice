b9a079f1b2fd0b5f9911b1579b89a77b9fa32fb7
koguchi-e <koguchi.e915@gmail.com>
2026-03-30 14:42:51 +0900

    演習2解答

--- src/components/Exercise2.jsx
+++ src/components/Exercise2.jsx
@@ -1,9 +1,16 @@
+import { devices } from "../data/data";
+
 const Exercise2 = () => {
-  return (
-    <ul>
-      <li>ここに適切なリストを表示してください</li>
-    </ul>
-  )
+  const listDevices = devices.map((device) => {
+    if (device.width > 700) {
+      if (device.price > 1000) {
+        return <li key={device.id}>{device.name}💰 高級品</li>;
+      } else {
+        return <li key={device.id}>{device.name}🛒 お買い得</li>;
+      }
+    }
+  });
+  return <ul>{listDevices}</ul>;
 };
 
 export default Exercise2;
