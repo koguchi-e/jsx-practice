8be03ffd87644dab9281c147d9ed00d3f5309910
koguchi-e <koguchi.e915@gmail.com>
2026-03-30 14:49:53 +0900

    演習2も以上に変更

--- src/components/Exercise2.jsx
+++ src/components/Exercise2.jsx
@@ -2,8 +2,8 @@ import { devices } from "../data/data";
 
 const Exercise2 = () => {
   const listDevices = devices.map((device) => {
-    if (device.width > 700) {
-      if (device.price > 1000) {
+    if (device.width >= 700) {
+      if (device.price >= 1000) {
         return <li key={device.id}>{device.name}💰 高級品</li>;
       } else {
         return <li key={device.id}>{device.name}🛒 お買い得</li>;
