661f4a7195a87a9bd479ae7addb538517dddfa17
koguchi-e <koguchi.e915@gmail.com>
2026-03-30 14:36:45 +0900

    演習1完成

--- src/components/Exercise1.jsx
+++ src/components/Exercise1.jsx
@@ -1,9 +1,15 @@
+import { users } from "../data/data";
+
 const Exercise1 = () => {
-  return (
-    <ul>
-      <li>ここに適切なリストを表示してください</li>
-    </ul>
-  )
+  const listUsers = users.map((user) => {
+    if (user.age > 18) {
+      return <li>{user.name}（成人）</li>;
+    } else {
+      return <li>{user.name}</li>;
+    }
+  });
+
+  return <ul>{listUsers}</ul>;
 };
 
 export default Exercise1;
