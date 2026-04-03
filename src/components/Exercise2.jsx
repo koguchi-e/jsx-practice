d1060c2906d7baff3215c751a9883fce2bbe0399
koguchi-e <koguchi.e915@gmail.com>
2026-04-03 16:27:25 +0900

    画面幅の絞り込みと金額の表記を分けて行うよう変更

--- src/components/Exercise2.jsx
+++ src/components/Exercise2.jsx
@@ -1,16 +1,15 @@
 import { devices } from "../data/data";
 
 const Exercise2 = () => {
-  const listDevices = devices.map((device) => {
-    if (device.width >= 700) {
-      if (device.price >= 1000) {
-        return <li key={device.id}>{device.name}💰 高級品</li>;
-      } else {
-        return <li key={device.id}>{device.name}🛒 お買い得</li>;
-      }
-    }
-  });
-  return <ul>{listDevices}</ul>;
+  const wideDevices = devices.filter((device) => device.width >= 700);
+  const listItem = wideDevices.map((device) => (
+    <li key={device.id}>
+      {device.price >= 1000
+        ? device.name + "💰 高級品"
+        : device.name + "🛒 お買い得"}
+    </li>
+  ));
+  return <ul>{listItem}</ul>;
 };
 
 export default Exercise2;
